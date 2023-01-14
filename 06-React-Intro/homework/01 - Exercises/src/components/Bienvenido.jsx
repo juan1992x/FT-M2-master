1a6e3b7b9e07f6573319e1246d58087b036103d6
juan1992x <juan.oliva1992@hotmail.com>
2023-01-13 21:47:16 -0500

    five commit

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,30 @@
 import React from "react";
+import Botones from "./botones.jsx";
 
-const studentName = "";
+
+
+
+
+const studentName = "juan";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
   // el código de tu componente acá
-  return;
+  return(
+    <div>
+      <h1>soy Henry!</h1>
+      <h3>{studentName}</h3>
+      <ul>
+         {techSkills.map(skill => 
+         <li key={skill}>
+          {skill}          
+          </li>)}
+      </ul>
+       <Botones alerts={alerts}/>
+
+    </div>
+  )
 }
 
 // Esto lo exportamos para los tests
